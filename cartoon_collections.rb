6c14d88d49b2e33bdeea7e6666722373c49a07c6
costamay <titusouko@gmail.com>
2022-05-15 22:39:45 +0300

    Completed assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarfs_array)# code an argument here
   # Your code here
+  dwarfs_array.each.with_index(1) { |dwarf, index| puts "#{index}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.map {|planet| planet.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  calls.any? {|call| call.size > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese_list)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_list.find do |cheese|
+    cheese_types.include?(cheese)
+  end
+
 end
